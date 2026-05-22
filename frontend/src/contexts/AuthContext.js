f59df0efcb388247ee0ca48832af6aa7e924352d
buttuura <132567158+buttuura@users.noreply.github.com>
2026-05-22 14:28:55 +0300

    Update AuthContext.js

--- frontend/src/contexts/AuthContext.js
+++ frontend/src/contexts/AuthContext.js
@@ -113,7 +113,7 @@ export const AuthProvider = ({ children }) => {
     getAuthHeaders,
     isAuthenticated: !!user,
     isAdmin: user?.role === 'admin' || user?.role === 'super_admin' || user?.role === 'treasurer',
-    isSuperAdmin: user?.role === 'super_admin' || user?.role === 'treasurer',
+    isTreasurer: user?.role === 'super_admin' || user?.role === 'treasurer',
     isPremium: user?.membership_type === 'premium',
   };
 
